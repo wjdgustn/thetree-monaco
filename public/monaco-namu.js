e7cc1fbbbfbc61149002c353ce4ea4ea905b8c6f
HYONSU <admin@hyonsu.com>
2025-03-11 03:42:51 +0900

    onLoad 핸들러에서 에디터 로드

--- public/monaco-namu.js
+++ public/monaco-namu.js
@@ -9,71 +9,73 @@ document.addEventListener('thetree:pageLoad', () => {
         else valueCache = value;
     }
 
-    const target = document.getElementById('thetree-monaco-area');
-    require.config({
-        paths: {
-            'vs': '/plugins/thetree-monaco/vs',
-            'namu': '/plugins/thetree-monaco/namu',
-        },
-    });
-    require.config({
-        'vs/nls' : {
-            availableLanguages: {
-                '*': 'ko'
-            }
-        }
-    });
-    require(['vs/editor/editor.main', 'namu/toolbar/quickaccess'], async () => {
-        const { default: namumark_register } = await import('/plugins/thetree-monaco/namu/vs/languages/namumark.js');
-        namumark_register(monaco);
-        monaco_editor = monaco.editor.create(target, {
-            language: 'namumark',
-            automaticLayout: true,
-            wordWrap: true,
-            renderWhitespace: 'all',
-            fontFamily: 'D2Coding, Consolas, "나눔고딕코딩", "Courier New", monospace',
-            value: valueCache,
-            minimap: {
-                enabled: false
+    tab._thetree.editor.onLoad = () => {
+        const target = document.getElementById('thetree-monaco-area');
+        require.config({
+            paths: {
+                'vs': '/plugins/thetree-monaco/vs',
+                'namu': '/plugins/thetree-monaco/namu',
+            },
+        });
+        require.config({
+            'vs/nls' : {
+                availableLanguages: {
+                    '*': 'ko'
+                }
             }
         });
-
-        const quickaccess = new namu.toolbar.QuickAccess(monaco_editor);
-        tab._thetree.editor.quickaccess = quickaccess;
-
-        const buttons = [...document.getElementsByClassName('plugin-thetree-monaco-button')];
-        for(let button of buttons) {
-            button.addEventListener('click', () => {
-                quickaccess.apply({
-                    bold: { bracket: `'''` },
-                    italic: { bracket: `''` },
-                    strike: { bracket: `~~` },
-                    link: {
-                        bracket: {
-                            open: `[[`,
-                            close: `]]`
-                        }
-                    },
-                    file: {
-                        bracket: {
-                            open: `[[파일:`,
-                            close: `]]`
-                        }
-                    },
-                    footnote: {
-                        bracket: {
-                            open: `[* `,
-                            close: `]`
-                        }
-                    },
-                    include: {
-                        bracket: {
-                            open: `[include(`,
-                            close: `)]`
-                        }
-                    }
-                }[button.dataset.name]);
+        require(['vs/editor/editor.main', 'namu/toolbar/quickaccess'], async () => {
+            const { default: namumark_register } = await import('/plugins/thetree-monaco/namu/vs/languages/namumark.js');
+            namumark_register(monaco);
+            monaco_editor = monaco.editor.create(target, {
+                language: 'namumark',
+                automaticLayout: true,
+                wordWrap: true,
+                renderWhitespace: 'all',
+                fontFamily: 'D2Coding, Consolas, "나눔고딕코딩", "Courier New", monospace',
+                value: valueCache,
+                minimap: {
+                    enabled: false
+                }
             });
-        }
-    });
+
+            const quickaccess = new namu.toolbar.QuickAccess(monaco_editor);
+            tab._thetree.editor.quickaccess = quickaccess;
+
+            const buttons = [...document.getElementsByClassName('plugin-thetree-monaco-button')];
+            for(let button of buttons) {
+                button.addEventListener('click', () => {
+                    quickaccess.apply({
+                        bold: { bracket: `'''` },
+                        italic: { bracket: `''` },
+                        strike: { bracket: `~~` },
+                        link: {
+                            bracket: {
+                                open: `[[`,
+                                close: `]]`
+                            }
+                        },
+                        file: {
+                            bracket: {
+                                open: `[[파일:`,
+                                close: `]]`
+                            }
+                        },
+                        footnote: {
+                            bracket: {
+                                open: `[* `,
+                                close: `]`
+                            }
+                        },
+                        include: {
+                            bracket: {
+                                open: `[include(`,
+                                close: `)]`
+                            }
+                        }
+                    }[button.dataset.name]);
+                });
+            }
+        });
+    }
 }, { once: true });
