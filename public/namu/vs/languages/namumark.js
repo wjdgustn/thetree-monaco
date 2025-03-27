f71e603f017a40d168544253966f346b5265b599
HYONSU <admin@hyonsu.com>
2025-03-28 01:54:42 +0900

    편집기 if문 지원

--- public/namu/vs/languages/namumark.js
+++ public/namu/vs/languages/namumark.js
@@ -1,17 +1,17 @@
 /*
  * NamuMark Language Definition for monaco-editor
  * Copyright (C) 2019 umanle S.R.L.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -198,6 +198,7 @@ export default function(monaco) {
                         '$3==latex': ['keyword', 'delimiter', {token: 'attribute.value', next: '@codeWithType.$3', nextEmbedded: 'latex', bracket: '@open'}],
                         '$3==wiki': ['keyword', 'delimiter', {token: 'attribute.value', next: '@codeWikiAttributes', bracket: '@open'}],
                         '$3==folding': ['keyword', 'delimiter', {token: 'attribute.value', next: '@codeWikiAttributes', bracket: '@open'}],
+                        '$3==if': ['keyword', 'delimiter', {token: 'attribute.value', next: '@codeWiki.$3', bracket: '@open'}],
                         '@default': ['keyword', 'white', {token: 'white', next: '@code', bracket: '@open'}],
                     }
                 }],
